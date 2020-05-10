7e57c31647ebab6f69ad8b7039c070c98e758023
qwerhacks <arjun.subramonian@gmail.com>
2020-05-10 09:35:48 -0700

    Grover + DJ with comments and scalability measure

--- PyQuil/deutsch_jozsa.py
+++ PyQuil/deutsch_jozsa.py
@@ -6,14 +6,35 @@ from pyquil.api import local_forest_runtime
 import sys
 import numpy as np
 import itertools
-import math
+import time
+import matplotlib.pyplot as plt
 
+# constant 0 function
 def zero(x):
 	return 0
 
-def xor(x):
+# constant 1 function
+def one(x):
+	return 1
+
+# XNOR-reduce
+# balanced
+def xnor(x):
 	return sum(x) % 2
 
+# XOR-reduce
+# balanced
+def xor(x):
+	return (sum(x) + 1) % 2 
+
+# U_f | x > | b > = | x > | b + f(x) >
+# preserve the state of the first n qubits
+# if f(x) == 0, we preserve the state of the helper qubit b
+# otherwise, if f(x) == 1, we invert the state of the helper qubit
+# U_f is just a 2^n by 2^n matrix where the "diagonal" consists of 2 by 2 blocks that are either the identity or Pauli X
+# the i-th diagonal block corresponds to the i-th possible n-bit input x_i to f
+## it is I if f(x_i) == 0
+## it is X if f(x_i) == 1
 def get_U_f(f, n):
 	U_f = np.zeros((2 ** (n + 1), 2 ** (n + 1)))
 	for idx, inputs in enumerate(list(itertools.product([0, 1], repeat=n))):
@@ -27,25 +48,43 @@ def get_U_f(f, n):
 
 	return U_f
 
+# generates the quantum circuit for Deutsch-Jozsa given U_f and n (the length of input bit strings)
 def dj_program(U_f, n):
 	p = Program()
 
+	# invert the helper qubit to make it 1
 	p += X(n)
+	# apply Hadamard to all input qubits and helper qubit
 	for i in range(n + 1):
 		p += H(i)
 
+	# define the U_f gate based on the unitary matrix returned by get_U_f
 	U_f_def = DefGate("U_f", U_f)
 	U_f_GATE = U_f_def.get_constructor()
 	p += U_f_def
-	p += U_f_GATE(*tuple(range(n + 1)))
+	p += U_f_GATE(*range(n + 1))
 	
+	# apply Hadamard to all input qubits
 	for i in range(n):
 		p += H(i)
 
-	print(p)
+	# print(p)
 	return p
 
-def print_results(result, trials, n):
+# pretty print results
+def print_results(test_name, result, exec_time, trials, n):
+
+	print()
+	print()
+	print('===================================')
+	print()
+	print('Test:', test_name)
+	print('Execution time:', exec_time, 'sec')
+	print()
+	print('===================================')
+	print('===================================')
+	print()
+
 	for t in range(trials):
 			verdict = None
 
@@ -66,22 +105,58 @@ def print_results(result, trials, n):
 			print()
 			print()
 
-# Discuss your effort to test the two programs and present results from the testing.  Discuss whether different cases of U_f lead to different execution times.
+# Discuss your effort to test the two programs and present results from the testing.
+# Discuss whether different cases of U_f lead to different execution times.
 # What is your experience with scalability as n grows?  Present a diagram that maps n to execution time.
+
+# File "/Users/arjuns/Documents/UCLA/Classes/Spring 2020/CS 239/venv/lib/python3.6/site-packages/rpcq/_client.py", line 205, in call
+#     raise utils.RPCError(reply.error)
+# rpcq._utils.RPCError: Unhandled error in host program:
+# The assertion
+# (NOT
+#  #1=(TYPEP (FIRST (CL-QUIL::GOVERNED-QUEUE-CONTENTS CL-QUIL::SUBGOVERNOR))
+#            'CL-QUIL:APPLICATION))
+# failed with #1# = T.
 
 if __name__ == '__main__':
 	n = int(sys.argv[1])
+	trials = int(sys.argv[2])
 
 	with local_forest_runtime():
 		qc = get_qc('9q-square-qvm')
+		qc.compiler.client.timeout = 10000
 
+		start_time = time.time()
 		p = dj_program(get_U_f(zero, n), n)
-		result = qc.run_and_measure(p, trials=10)
-		print_results(result, 10, n)
+		result = qc.run_and_measure(p, trials=trials)
+		print_results('Constant 0', result, time.time() - start_time, trials, n)
 
+		start_time = time.time()
+		p = dj_program(get_U_f(one, n), n)
+		result = qc.run_and_measure(p, trials=trials)
+		print_results('Constant 1', result, time.time() - start_time, trials, n)
+
+		start_time = time.time()
+		p = dj_program(get_U_f(xnor, n), n)
+		result = qc.run_and_measure(p, trials=trials)
+		print_results('XNOR-reduce', result, time.time() - start_time, trials, n)
+
+		start_time = time.time()
 		p = dj_program(get_U_f(xor, n), n)
-		result = qc.run_and_measure(p, trials=10)
-		print_results(result, 10, n)
+		result = qc.run_and_measure(p, trials=trials)
+		print_results('XOR-reduce', result, time.time() - start_time, trials, n)
+
+		exec_times = []
+		for n_test in [1, 2, 4, 8]:
+			start_time = time.time()
+			p = dj_program(get_U_f(one, n_test), n_test)
+			result = qc.run_and_measure(p, trials=1)
+			exec_times.append(time.time() - start_time)
+		plt.plot([1, 2, 4, 8], exec_times)
+		plt.xlabel('Number of qubits')
+		plt.ylabel('Execution time (in seconds)')
+		plt.title('Scalability as number of qubits grows')
+		plt.savefig('deutsch_jozsa_scalability.png')
 
 
 
--- PyQuil/grover.py
+++ PyQuil/grover.py
@@ -7,74 +7,153 @@ import sys
 import numpy as np
 import itertools
 import math
+import time
+import matplotlib.pyplot as plt 
 
+# Returns 1 only in the case that x is an n-bit string of all 1's
 def f(x):
 	return int(sum(x) == len(x))
 
+# Returns 1 only in the case that x is an n-bit string of all 0's
 def g(x):
 	return int(sum(x) == 0)
 
+# Returns 1 when x has an odd number of 1's (same as XNOR-reduce)
+def h(x):
+	return sum(x) % 2
+
+# Doesn't return 1 for any inputs (same as Constant 0)
 def zero(x):
 	return 0
 
+# Z_f is just a 2^n by 2^n diagonal matrix
+# the i-th entry in the diagonal corresponds to the 2^i-th possible n-bit input x and is equal to (-1)^f(x)
+# hence, | x > gets mapped to (-1)^f(x) | x >
 def get_Z_f(f, n):
 	return np.diag([(-1) ** (f(x)) for x in list(itertools.product([0, 1], repeat=n))])
 
+# Z_0 is just a 2^n by 2^n diagonal matrix
+# the i-th entry in the diagonal corresponds to the 2^i-th possible n-bit input x and is equal to -1 if x == {0}^n and 1 otherwise
+# hence, | x > gets mapped to -| x > if x == {0}^n and | x > otherwise
 def get_Z_0(n):
 	Z_0 = np.eye(2 ** n)
 	Z_0[0, 0] = -1
 	return Z_0
 
+# generates the quantum circuit for Grover's algorithm given Z_f, Z_0, and n (the length of input bit strings)
 def grover_program(Z_f, Z_0, n):
 	p = Program()
+
+	# apply Hadamard to all qubits
 	for i in range(n):
 		p += H(i)
 
+	# define the Z_f gate based on the unitary matrix returned by get_Z_f
 	Z_f_def = DefGate("Z_f", Z_f)
 	Z_f_GATE = Z_f_def.get_constructor()
 	p += Z_f_def
 
+	# define the Z_0 gate based on the unitary matrix returned by get_Z_0
 	Z_0_def = DefGate("Z_0", Z_0)
 	Z_0_GATE = Z_0_def.get_constructor()
 	p += Z_0_def
 
-	neg_I_def = DefGate("neg_I", -1 * np.eye(2 ** n))
+	# define the neg_I gate, which flips the sign of a qubit
+	# achieves multiplication by -1 in the G operator (as defined in the lecture notes)
+	# not necessary, since global phases are irrelevant, but just for consistency with lecture notes
+	neg_I_def = DefGate("neg_I", -1 * np.eye(2))
 	NEG_I_GATE = neg_I_def.get_constructor()
 	p += neg_I_def
 
+	# iterate floor of pi/4 * sqrt(2^n) times, as prescribed
+	# on each iteration, apply the G operator
 	for i in range(math.floor(math.pi / 4 * math.sqrt(2 ** n))):
-		p += Z_f_GATE(*tuple(range(n)))
-		for j in range(n):
-			p += H(j)
-		p += Z_0_GATE(*tuple(range(n)))
-		for j in range(n):
-			p += H(j)
-		p += NEG_I_GATE(*tuple(range(n)))
+		# apply Z_f gate
+		p += Z_f_GATE(*range(n))
 
-	print(p)
+		# apply Hadamard to all qubits
+		for j in range(n):
+			p += H(j)
+
+		# apply Z_0 gate
+		p += Z_0_GATE(*range(n))
+
+		for j in range(n):
+			p += H(j)
+
+		# take negative
+		p += NEG_I_GATE(0)
+
+	# print(p)
 	return p
 
-# Discuss your effort to test the two programs and present results from the testing.  Discuss whether different cases of U_f lead to different execution times.
+# pretty print results
+def print_results(test_name, func, result, exec_time, trials, n):
+
+	print()
+	print()
+	print('===================================')
+	print()
+	print('Test:', test_name)
+	print('Execution time:', exec_time, 'sec')
+	print()
+	print('===================================')
+	print('===================================')
+	print()
+
+	for t in range(trials):
+			print('===================================')
+			print()
+			print('Trial', t + 1)
+			print('x =', ''.join([str(result[i][t]) for i in range(n)]))
+			print('f(x) =', func([result[i][t] for i in range(n)]))
+			print()
+			print('===================================')
+			print()
+			print()
+
+# Discuss your effort to test the two programs and present results from the testing.
+# Discuss whether different cases of U_f lead to different execution times.
 # What is your experience with scalability as n grows?  Present a diagram that maps n to execution time.
 
+# test driver
 if __name__ == '__main__':
 	n = int(sys.argv[1])
+	trials = int(sys.argv[2])
 
 	with local_forest_runtime():
 		qc = get_qc('9q-square-qvm')
+		qc.compiler.client.timeout = 10000
 
+		start_time = time.time()
 		p = grover_program(get_Z_f(f, n), get_Z_0(n), n)
-		result = qc.run_and_measure(p, trials=10)
-		for i in range(n):
-			print(result[i])
+		result = qc.run_and_measure(p, trials=trials)
+		print_results("All 1's", f, result, time.time() - start_time, trials, n)
 
+		start_time = time.time()
 		p = grover_program(get_Z_f(g, n), get_Z_0(n), n)
-		result = qc.run_and_measure(p, trials=10)
-		for i in range(n):
-			print(result[i])
+		result = qc.run_and_measure(p, trials=trials)
+		print_results("All 0's", g, result, time.time() - start_time, trials, n)
 
+		start_time = time.time()
+		p = grover_program(get_Z_f(h, n), get_Z_0(n), n)
+		result = qc.run_and_measure(p, trials=trials)
+		print_results("XNOR-reduce", h, result, time.time() - start_time, trials, n)
+
+		start_time = time.time()
 		p = grover_program(get_Z_f(zero, n), get_Z_0(n), n)
-		result = qc.run_and_measure(p, trials=10)
-		for i in range(n):
-			print(result[i])
+		result = qc.run_and_measure(p, trials=trials)
+		print_results("Constant 0", zero, result, time.time() - start_time, trials, n)
+
+		exec_times = []
+		for n_test in [1, 2, 4, 8]:
+			start_time = time.time()
+			p = grover_program(get_Z_f(f, n_test), get_Z_0(n_test), n_test)
+			result = qc.run_and_measure(p, trials=1)
+			exec_times.append(time.time() - start_time)
+		plt.plot([1, 2, 4, 8], exec_times)
+		plt.xlabel('Number of qubits')
+		plt.ylabel('Execution time (in seconds)')
+		plt.title('Scalability as number of qubits grows')
+		plt.savefig('grover_scalability.png')
 
