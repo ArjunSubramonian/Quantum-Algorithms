7c469b1d1b59241f79486573682a80c5eded268d
Siddarth Chalasani <darthsid2000@ucla.edu>
2020-05-13 09:55:14 -0700

    Added new functions to func.py for Bernstein-Vazirani

--- PyQuil/func.py
+++ PyQuil/func.py
@@ -25,3 +25,31 @@ def all_ones(x):
 # Returns 1 only in the case that x is an n-bit string of all 0's
 def all_zeros(x):
 	return int(sum(x) == 0)
+
+
+# The following functions were designed to be used in the Bernstein-Vazirani algorithm
+# Additionally, the zero() and one() function from above also work
+
+# Returns only first bit
+def first_bit(x):
+        return x[0]
+
+# Returns only last bit
+def last_bit(x):
+	return x[-1]
+
+#Returns sum of bit string plus 1 (mod 2)
+def add_one(x):
+        return (sum(x)+1)%2
+
+#Returns sum of bit string (mod 2)
+def add(x):
+	return sum(x) % 2
+
+#Returns sum of bottom half of bit string (mod 2)
+def bottom_half(x):
+	return sum(x[(len(x)//2):]) % 2
+
+#Returns sum of top half of bit string (mod 2)
+def top_half(x):
+	return sum(x[:(len(x)//2)]) % 2
