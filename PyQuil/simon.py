ae0c140e5d1d97e228b37f4b206ac7fb3aed41c7
qwerhacks <arjun.subramonian@gmail.com>
2020-05-14 13:54:56 -0700

    README and report added

--- PyQuil/simon.py
+++ PyQuil/simon.py
@@ -18,7 +18,6 @@ from func import *
 # preserve the state of the first n qubits
 # compute all possible bit strings of size 2*n and create dictionary of each input paired with its index
 # create another dictionary of each output value (f(x) mod2 b) paired with its index
-# U_f is just a 2^(2*n) by 2^(2*n) matrix where the "diagonal" consists of 2 by 2 blocks that are either the identity or Pauli X
 # the i-th diagonal block corresponds to the i-th possible n-bit input x_i to f
 # The indices mapped to the input values represent the rows of U_f. 
 # Indices mapped to the output values represent the columns of U_f.
@@ -62,7 +61,6 @@ def get_U_f(f,n):
     #print (U_f)
     return U_f
 
-# generates the quantum circuit for Deutsch-Jozsa given U_f and n (the length of input bit strings)
 def simon_program(U_f, n):
     p = Program()
 
@@ -89,7 +87,6 @@ def simon_program(U_f, n):
 def constraint_solver(list_y, n):
     # we need to solve the linear system given by y . s = 0, for n-1 equations
     s = [1] * n
-    
     for i in list_y:
         for j in range(n):
             if i[j] == 1 and s[j] == 1:
@@ -134,9 +131,8 @@ if __name__ == '__main__':
     if len(sys.argv) <= 1:
         print('\nLook in func.py for a function name to pass in as an argument, followed by the length of the bit string and the number of trials.\nAlternatively, pass in the function name followed by \'--graph\' to create of graph of the scalability of the chosen function.\nRefer to README for additional info.\n')
         exit()
+
     graph = False
-    if sys.argv[2] == '--graph':
-        graph = True
 
     func_in_name = sys.argv[1]
     try:
@@ -144,9 +140,9 @@ if __name__ == '__main__':
     except AttributeError:
         raise NotImplementedError("Class `{}` does not implement `{}`".format(func.__class__.__name__, func_in_name))
     
-    if not graph:
-        n = int(sys.argv[2])
-        trials = int(sys.argv[3]) # number of 4-cycle iterations of circuit
+  
+    n = int(sys.argv[2])
+    trials = int(sys.argv[3]) # number of 4-cycle iterations of circuit
 
    
     with local_forest_runtime():
@@ -161,13 +157,13 @@ if __name__ == '__main__':
                 list_indep_y = []
                 start_time = time.time()
                 for j in range(4*trials):
-                    for iter in range(n_test-1):
+                    for iter in range(n_test):
                 
                         U_f = get_U_f(func_in, n_test)
                         p = simon_program(U_f, n_test)
                         result = qc.run_and_measure(p, 1)
                     
-                        print(result)
+                        #print(result)
 
                         #appending result to list of y's later to be used for solving for s
                         #only append if result is not all 0s
@@ -197,56 +193,4 @@ if __name__ == '__main__':
             timepassed = time.time() - start_time
             exec_times.append(timepassed)
 
-            print_results(func_in_name, s_test, timepassed, n_test)
-
             print('s_test: ', s_test)
-
-        if graph:
-            exec_times = []
-            trials = 5
-            for n_test in sorted([int(arg) for arg in sys.argv[3:]]): 
-                rank = 0
-                list_indep_y = []
-                start_time = time.time()
-                for j in range(4*trials):
-                    for iter in range(n_test-1):
-                
-                            U_f = get_U_f(func_in, n_test)
-                            p = simon_program(U_f, n_test)
-                            result = qc.run_and_measure(p, 1)
-
-                            #appending result to list of y's later to be used for solving for s
-                            #only append if result is not all 0s
-                            allzero = True
-                            for h in range(n_test):
-                                if result[h] == 1:
-                                    allzero = False
-                                    break
-
-                            if allzero == False:
-                                list_indep_y.append(result)
-                            else:
-                                allzero_y = result
-                    
-                    #create matrix of y's
-                    y_matrix = np.array(list_indep_y)
-                    rank = np.linalg.matrix_rank(y_matrix)
-
-                    #stop iterating through loop setup once there are n-1 linearly independent y's
-                    if rank == n_test-1:
-                        break
-            
-                #solve for s
-                if rank == 0:
-                    list_indep_y.append(allzero_y)
-                s_test = constraint_solver(list_indep_y, n_test)
-                timepassed = time.time() - start_time
-                exec_times.append(timepassed)
-
-                print('s_test: ', s_test)
-            plt.figure()
-            plt.plot(sorted([int(arg) for arg in sys.argv[3:]]), exec_times)
-            plt.xlabel('Number of qubits')
-            plt.ylabel('Execution time (in seconds)')
-            plt.title('Scalability as number of qubits grows for Simon on %s' % func_in_name)
-            plt.savefig('simon_scalability_%s_{:%Y-%m-%d_%H-%M-%S}.png'.format(datetime.datetime.now()) % func_in_name)
