73985c59fbf4cb51c7ce323513f7cd6aa56f1576
Siddarth Chalasani <darthsid2000@ucla.edu>
2020-05-14 01:54:27 -0700

    Updated BV with --graph option

--- PyQuil/bernstein_vazirani.py
+++ PyQuil/bernstein_vazirani.py
@@ -21,25 +21,31 @@ from func import *
 # the i-th diagonal block corresponds to the i-th possible n-bit input x_i to f
 ## it is I if f(x_i) == 0
 ## it is X if f(x_i) == 1
+# i.e., it is X^f(x_i)
 def get_U_f(f, n):
 	U_f = np.zeros((2 ** (n + 1), 2 ** (n + 1)))
 	for idx, inputs in enumerate(list(itertools.product([0, 1], repeat=n))):
 		output = f(inputs)
+		
+		# the 2x2 box on the diagonal is I
 		if output == 0:
 			U_f[2 * idx, 2 * idx] = 1
 			U_f[2 * idx + 1, 2 * idx + 1] = 1
+		
+		# the 2x2 box on th diagonal is Pauli X
 		elif output == 1:
 			U_f[2 * idx, 2 * idx + 1] = 1
 			U_f[2 * idx + 1, 2 * idx] = 1
 
 	return U_f
 
-# generates the quantum circuit for Deutsch-Jozsa given U_f and n (the length of input bit strings)
+# generates the quantum circuit for Bernstein-Vazirani given U_f and n (the length of input bit strings)
 def bv_program(U_f, n):
 	p = Program()
 
 	# invert the helper qubit to make it 1
 	p += X(n)
+	
 	# apply Hadamard to all input qubits and helper qubit
 	for i in range(n + 1):
 		p += H(i)
@@ -54,7 +60,7 @@ def bv_program(U_f, n):
 	for i in range(n):
 		p += H(i)
 
-	# print(p)
+	#print(p)
 	return p
 
 # pretty print results
@@ -85,49 +91,47 @@ def print_results(test_name, result, exec_time, trials, n, b):
 			print()
 			print()
 
-# Discuss your effort to test the two programs and present results from the testing.
-# Discuss whether different cases of U_f lead to different execution times.
-# What is your experience with scalability as n grows?  Present a diagram that maps n to execution time.
-
-# File "/Users/arjuns/Documents/UCLA/Classes/Spring 2020/CS 239/venv/lib/python3.6/site-packages/rpcq/_client.py", line 205, in call
-#     raise utils.RPCError(reply.error)
-# rpcq._utils.RPCError: Unhandled error in host program:
-# The assertion
-# (NOT
-#  #1=(TYPEP (FIRST (CL-QUIL::GOVERNED-QUEUE-CONTENTS CL-QUIL::SUBGOVERNOR))
-#            'CL-QUIL:APPLICATION))
-# failed with #1# = T.
 
 if __name__ == '__main__':
 
-	if len(sys.argv) <= 1:
-		print('Look in func.py for a function name to pass in as an argument, followed by the length of the bit string and the number of trials.')
+	if len(sys.argv) <= 1 or len(sys.argv) > 4:
+		print('\nLook in func.py for a function name to pass in as an argument, followed by the length of the bit string and the number of trials.\nAlternatively, pass in the function name followed by \'--graph\' to create of graph of the scalability of the chosen function.\nRefer to README for additional info.\n')
 		exit()
+	graph = False
+	if sys.argv[2] == '--graph':
+		graph = True
 	func_in_name = sys.argv[1]
 	try:
-	    func_in = getattr(func, func_in_name)
+		func_in = getattr(func, func_in_name)
 	except AttributeError:
-	    raise NotImplementedError("Class `{}` does not implement `{}`".format(func.__class__.__name__, func_in_name))
-	n = int(sys.argv[2])
-	trials = int(sys.argv[3])
+		raise NotImplementedError("Class `{}` does not implement `{}`".format(func.__class__.__name__, func_in_name))
+	if not graph:
+		n = int(sys.argv[2])
+		trials = int(sys.argv[3])
 
 	with local_forest_runtime():
-		qc = get_qc('9q-square-qvm')
+		qc = get_qc('16q-qvm', noisy = False)
 		qc.compiler.client.timeout = 10000
-		b = func_in([0]*n)
-		U_f = get_U_f(func_in, n)
-		start_time = time.time()
-		p = bv_program(U_f, n)
-		result = qc.run_and_measure(p, trials=trials)
-		print_results(func_in_name, result, time.time() - start_time, trials, n, b)
-		'''
-		exec_times = []
-		for n_test in [1, 2, 3, 4]:
-			U_f = get_U_f(func_in, n_test)
+		
+		if not graph:
+			b = func_in([0]*n)
+			U_f = get_U_f(func_in, n)
 			start_time = time.time()
-			p = bv_program(U_f, n_test)
-			result = qc.run_and_measure(p, trials=1)
-			exec_times.append(time.time() - start_time)
-			
-		print(*exec_times, sep = ' s\t')
-		'''
+			p = bv_program(U_f, n)
+			result = qc.run_and_measure(p, trials=trials)
+			print_results(func_in_name, result, time.time() - start_time, trials, n, b)
+		
+		if graph:
+			exec_times = []
+			for n_test in [1, 2, 3, 4]:
+				U_f = get_U_f(func_in, n_test)
+				start_time = time.time()
+				p = bv_program(U_f, n_test)
+				result = qc.run_and_measure(p, trials=1)
+				exec_times.append(time.time() - start_time)
+			plt.figure()
+			plt.plot([1,2,3,4], exec_times)
+			plt.xlabel('Number of Qubits')
+			plt.ylabel('Execution time (sec)')
+			plt.title('Scalability of Bernstein-Vazirani on %s' % func_in_name)
+			plt.savefig('bernstein_vazirani_scalability_%s_{:%Y-%m-%d_%H-%M-%S}.png'.format(datetime.datetime.now()) % func_in_name)	
